ec2caf1b30ad03d4d12ad63e82c494272daea36b
Hemanth Bhagwat <153309030+Hemanthb-2314@users.noreply.github.com>
2024-08-01 18:40:37 +0530

    Delete CustRepClient.java

--- CustRepClient.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class CustRepClient {
-    public static void main(String[] args) {
-        new MainFrame();
-
-    }
-}
